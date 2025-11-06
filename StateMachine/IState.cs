ee2f3284ee0d11fccf8b878ef42a409fc4df8afe
Rioshe <rioshe.media@gmail.com>
2025-11-06 21:34:52 +0530

    Added statemachine

---
+++ StateMachine/IState.cs
@@ -0,0 +1,10 @@
+namespace RSG.StateMachine
+{
+    public interface IState
+    {
+        public void OnEnter();
+        public void OnUpdate();
+        public void OnFixedUpdate();
+        public void OnExit();
+    }
+}
---
+++ StateMachine/StateMachine.cs
@@ -0,0 +1,99 @@
+using System;
+
+namespace RSG.StateMachine
+{
+    public class StateMachine
+    {
+        private int m_currentState = -1;
+        private int m_targetState = -1;
+        private int m_lastState = -1;
+
+        private readonly IState[] m_states;
+        private readonly Action<int> m_debugStateChangeCallback;
+        
+        public StateMachine(Enum stateCount, Action<int> debugStateChangeCallback = null)
+        {
+            int count = Convert.ToInt32(stateCount);
+            m_states = new IState[count];
+            m_debugStateChangeCallback = debugStateChangeCallback;
+        }
+        
+        public void RegisterState(Enum stateEnum, IState state)
+        {
+            int stateInt = Convert.ToInt32(stateEnum); 
+            m_states[stateInt] = state;
+        }
+
+        public void SetState(Enum targetState)
+        {
+            int targetStateInt = Convert.ToInt32(targetState);
+            SetState(targetStateInt);
+        }
+        
+        public void SetState(int targetState)
+        {
+            if(targetState == m_currentState)
+                return;
+        
+            if (targetState >= 0)
+            {
+                m_targetState = targetState;
+
+#if PROJECT_DEBUG
+                m_debugStateChangeCallback?.Invoke(m_targetState);
+#endif
+            }
+        }
+
+        public int GetLastState()
+        {
+            return m_lastState;
+        }
+
+        public int GetCurrentState()
+        {
+            return m_currentState;
+        }
+
+        public bool IsState(Enum stateEnum)
+        {
+            int stateInt = Convert.ToInt32(stateEnum); 
+            return m_currentState == stateInt;
+        }
+
+        public IState GetState(Enum stateEnum)
+        {
+            int stateInt = Convert.ToInt32(stateEnum); 
+            return m_states[stateInt];
+        }
+
+        public void Update()
+        {
+            if (m_currentState != m_targetState)
+            {
+                if (m_currentState >= 0)
+                {
+                    m_states[m_currentState].OnExit();
+                }
+            
+                m_lastState = m_currentState;
+                m_currentState = m_targetState;
+                m_states[m_targetState].OnEnter();
+            }
+
+            if (m_currentState >= 0)
+            {
+                m_states[m_currentState].OnUpdate();
+            }
+        }
+
+        public void FixUpdate()
+        {
+            if (m_currentState >= 0)
+            {
+                m_states[m_currentState].OnFixedUpdate();
+            }
+        }
+        
+    }
+}
