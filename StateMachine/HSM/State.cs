fafb5d8c9bdebba2353c534bbe51fb94c3210d73
Rioshe <rioshe.media@gmail.com>
2025-11-13 23:09:20 +0530

    HSM

---
+++ StateMachine/HSM/State.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+
+namespace RSG.HierarchicalStateMachine
+{
+    public abstract class State
+    {
+        public readonly StateMachine Machine;
+        public readonly State ParentState;
+        public State ActiveChildState;
+
+        public State( StateMachine machine, State parentState )
+        {
+            Machine = machine;
+            ParentState = parentState;
+        }
+
+        protected virtual State GetInitialState() => null;
+        protected virtual State GetTransition() => null;
+        
+        
+        internal void Enter()
+        {
+            if( ParentState != null )
+                ParentState.ActiveChildState = this;
+            
+            OnEnter();
+            
+            State initialState = GetInitialState();
+            if( initialState != null )
+                initialState.Enter();
+        }
+        protected virtual void OnEnter(){}
+
+        
+        internal void Exit()
+        {
+            if(ActiveChildState != null)
+                ActiveChildState.Exit();
+            ActiveChildState =  null;
+            OnExit();
+        }
+        protected virtual void OnExit(){}
+
+        
+        internal void Update( float deltaTime )
+        {
+            State transition = GetTransition();
+            if( transition != null )
+            {
+                Machine.Sequencer.RequestTransition( this, transition );
+                return;
+            }
+            
+            if(ActiveChildState != null)
+                ActiveChildState.Update( deltaTime );
+            
+            OnUpdate( deltaTime );
+        }
+        protected virtual void OnUpdate(float deltaTime) {}
+
+
+        public State ActiveLeafState()
+        {
+            State leafState = this;
+            while(leafState.ActiveChildState != null)
+            {
+                leafState = leafState.ActiveChildState;
+            }
+            
+            return leafState;
+        }
+
+        public IEnumerable<State> PathToRootState()
+        {
+            for( State s = this; s != null; s = s.ParentState )
+            {
+                yield return s;
+            }
+        }
+    }
+}
---
+++ StateMachine/HSM/TransitionSequencer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace RSG.HierarchicalStateMachine
+{
+    public class TransitionSequencer
+    {
+        public readonly StateMachine Machine;
+
+        public TransitionSequencer( StateMachine machine )
+        {
+            Machine = machine;
+        }
+        
+        public void RequestTransition(State from, State to){}
+
+        public static State LowestCommonAncestor( State a, State b )
+        {
+            HashSet<State> states = new HashSet<State>();
+            for( State s = a; s != null; s = s.ParentState)
+            {
+                states.Add(s);
+            }
+
+            for( State s = b; s != null; s = s.ParentState )
+            {
+                if( states.Contains( s ) )
+                {
+                    return s;
+                }
+            }
+
+            return null;
+        }
+    }
+}
