82a159a7c4def8a068055f810da4b74333095081
Rioshe <rioshe.media@gmail.com>
2025-11-15 10:34:22 +0530

    added package installer

---
+++ Editor/PackageInstaller.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace RSG.Editor
+{
+    public static class PackageInstaller
+    {
+        [MenuItem("RSG/Install Essential Packages")]
+        public static void Install()
+        {
+            Debug.Log("<color=cyan>[RSG]</color> Installing essential packages...");
+
+            Install("https://github.com/Inspiaaa/UnityHFSM.git");
+            Install("com.demigiant.dotween");
+            Install("com.unity.in-game-debug-console"); // if in UPM registry
+            Install("https://github.com/Tayx94/graphy.git");
+
+            Debug.Log("<color=green>[RSG]</color> Essential packages installed!");
+        }
+
+        private static void Install(string package)
+        {
+            UnityEditor.PackageManager.Client.Add(package);
+            Debug.Log($"<color=yellow>[RSG]</color> Installing: {package}");
+        }
+    }
+}
