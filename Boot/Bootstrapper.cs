6d2199f2bba1ac6481a5ad125a248aad7e3d8544
Rioshe <rioshe.media@gmail.com>
2025-12-18 20:28:31 +0530

    00

--- Boot/Bootstrapper.cs
+++ Boot/Bootstrapper.cs
@@ -7,11 +7,9 @@ namespace RSG
     public class Bootstrapper : MonoBehaviour
     {
         [Header("Configuration")]
-        [Tooltip("Raised when all systems are loaded and ready.")]
         [SerializeField] private BootChannelSO m_bootChannelSo;
 
         [Header("Systems")]
-        [Tooltip("Core systems to spawn. Sorted by priority automatically.")]
         [SerializeField] private List<BootSystemBase> m_bootPrefabs = new List<BootSystemBase>();
 
 #if RSG_DEBUG
@@ -19,19 +17,20 @@ namespace RSG
         [SerializeField] private List<GameObject> m_debugPrefabs = new List<GameObject>();
 #endif
 
+        private readonly List<BootSystemBase> m_initializedSystems = new List<BootSystemBase>();
+
         private void Awake()
         {
             DontDestroyOnLoad(gameObject);
         }
 
-        private void Start()
+        private async void Start()
         {
             List<BootSystemBase> initializedSystems = new List<BootSystemBase>();
 
             foreach (BootSystemBase prefab in m_bootPrefabs)
             {
                 if (!prefab) continue;
-                
                 BootSystemBase instance = Instantiate(prefab, transform);
                 initializedSystems.Add(instance);
             }
@@ -40,7 +39,6 @@ namespace RSG
             foreach (GameObject prefab in m_debugPrefabs)
             {
                 if (!prefab) continue;
-
                 GameObject instance = Instantiate(prefab, transform);
                 if (instance.TryGetComponent(out BootSystemBase bootSystem))
                 {
@@ -49,14 +47,33 @@ namespace RSG
             }
 #endif
 
+            initializedSystems.Sort((a, b) => a.InitializationPriority.CompareTo(b.InitializationPriority));
+
             foreach (BootSystemBase system in initializedSystems)
             {
                 system.Initialize();
+                m_initializedSystems.Add(system);
+            }
+
+            foreach (BootSystemBase system in initializedSystems)
+            {
+                await system.InitializeAsync();
             }
 
             if (m_bootChannelSo)
             {
                 m_bootChannelSo.RaiseBootCompleteEvent();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            foreach (BootSystemBase system in m_initializedSystems)
+            {
+                if (system)
+                {
+                    system.Shutdown();
+                }
             }
         }
     }
